47e6e3d27d206cd2c076ff9329a0751bbbdc5731
Cheri M <cmerrym1@kent.edu>
2022-03-31 10:49:17 -0400

    save point

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,4 @@
+const { BADQUERY } = require('dns');
 const { User } = require('../models');
 
 const userController = {
@@ -31,12 +32,35 @@ const userController = {
     //createUser
     createUser({ body }, res) {
         User.create(body)
-        .then(dbUserData => this.res.json(dbUserData))
-        .catch(err => this.res.status(400).json(err));
-    }
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
 
-    // update user by id
-    
-  }
+    // update pizza by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+      .catch(err => res.status(400).json(err));
+  },
+
+    //delete user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+};
 
 module.exports = userController;
